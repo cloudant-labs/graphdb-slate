c5adf96e2f20e614ba7aa4fd6a61ddbea1e6f857
Max Thayer <garbados@gmail.com>
2014-04-29 16:38:46 -0400

    maybe stop quoting everything maybe maybe eh

--- scripts/deploy-master.sh
+++ scripts/deploy-master.sh
@@ -8,7 +8,7 @@ then
   then
     echo "No USERNAME provided. Skipping..."
   else
-    couchapp push couchapp 'https://$1:$2@garbados.cloudant.com/api-ref'
+    couchapp push couchapp https://$1:$2@garbados.cloudant.com/api-ref
   fi
 else
   echo "$TRAVIS_BRANCH is not $BRANCH, or this is a pull request; not deploying."
