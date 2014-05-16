b3fa94402403904c48dfc81d32c801b9479b5410
Max Thayer <garbados@gmail.com>
2014-05-16 12:44:31 -0400

    works with test data; now for live fire

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,2 +1,3 @@
 //= require './jquery_ui'
+//= require './typeahead.js'
 //= require_tree .
--- source/javascripts/search.js
+++ source/javascripts/search.js
@@ -1,22 +1,40 @@
-var res;
 $(function () {
   var search_box = '#searchbox form';
-  var query_input = search_box + '> input[name="search"]';
+  var query_input = search_box + ' input[name="search"]';
   var query_url = '_search/docs';
 
-  function on_submit (event) {
-    var query = $(query_input).val();
+  var typeahead_opts = {
+    minLength: 3,
+    highlight: true
+  };
 
-    $.get(query_url, { q: query })
-     .done(function (response) {
-      res = response;
-     })
-     .fail(function (jqXHR, status) {
-      // TODO
-     });
+  var typeahead_data = {
+    name: 'doc_matches',
+    source: compute_suggestions,
+    displayKey: function (row) {
+      return row.fields.title;
+    }
+  };
+
+  function compute_suggestions (query, cb) {
+    $.get(query_url, { q: query }).done(function (response) {
+      var res = JSON.parse(response);
+      cb(res.rows);
+    });
+  }
+
+  function on_submit (event) {
+    console.log(arguments);
+    var section_id = $(query_input).val().replace(' ', '-').toLowerCase();
+    console.log(section_id);
+    
+    $('html, body').animate({
+        scrollTop: $("#" + section_id).offset().top
+    }, 200);
 
     return false;
   }
 
+  $(query_input).typeahead(typeahead_opts, typeahead_data);
   $(search_box).submit(on_submit);
 });
