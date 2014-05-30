53fe7789eb55814237461818a99c3d43bd88a7b2
sean barclay <fudd1011@hotmail.com>
2014-05-29 23:25:06 -0700

    updating user account to point travis to docs-testa

--- scripts/deploy-master.sh
+++ scripts/deploy-master.sh
@@ -9,7 +9,7 @@ then
     echo "No USERNAME provided. Skipping..."
   else
     python scripts/upload_documentation_as_docs.py
-    couchapp push couchapp https://$1:$2@garbados.cloudant.com/api-ref
+    couchapp push couchapp https://$1:$2@docs-testa.cloudant.com/api-ref
   fi
 else
   echo "$TRAVIS_BRANCH is not $BRANCH, or this is a pull request; not deploying."
