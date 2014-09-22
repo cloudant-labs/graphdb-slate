9a540f58e07aac8826ed74472f8c98e159e2b2cc
Kim Stebel <kim.stebel@gmail.com>
2014-09-22 14:44:15 +0100

    more missing quotes...argh

--- scripts/deploy-master.sh
+++ scripts/deploy-master.sh
@@ -4,12 +4,12 @@ BRANCH=testb
 
 if [[ "$TRAVIS_BRANCH" == "$BRANCH" ]] && [[ "$TRAVIS_PULL_REQUEST" == "false" ]]
 then
-  if [ -z $1 ]
+  if [ -z "$1" ]
   then
     echo "No USERNAME provided. Skipping..."
   else
     python scripts/upload_documentation_as_docs.py
-    couchapp push couchapp https://$1:$2@docs-testb.cloudant.com/api-ref
+    couchapp push couchapp "https://$1:$2@docs-testb.cloudant.com/api-ref"
   fi
 else
   echo "$TRAVIS_BRANCH is not $BRANCH, or this is a pull request; not deploying."
