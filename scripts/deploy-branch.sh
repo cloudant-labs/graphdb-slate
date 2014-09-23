abc36f9c58c573f1d9b4a74aa8a9757cbf3b13e1
Kim Stebel <kim.stebel@gmail.com>
2014-09-23 14:20:22 +0100

    deploy any branch to docs-testb.cloudant.com/$branch

---
+++ scripts/deploy-branch.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+BRANCH=testb
+
+if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]
+then
+  if [ -z "$1" ]
+  then
+    echo "No USERNAME provided. Skipping..."
+  else
+    python scripts/upload_documentation_as_docs.py
+    #deploy with db name == branch name
+    couchapp push couchapp "https://$1:$2@docs-testb.cloudant.com/${TRAVIS_BRANCH}"
+    #give everybody read access and docs-testb admin access
+    curl -X PUT "https://$1:$2@docs-testb.cloudant.com/${TRAVIS_BRANCH}/_security" -d '{"_id":"_security","cloudant":{"nobody":["_reader"],"docs-testb":["_reader","_writer","_admin","_replicator"]}}'
+  fi
+else
+  echo "this is a pull request; not deploying."
+fi
