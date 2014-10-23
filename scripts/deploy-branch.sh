32d081143e4c11c110effd5c7b8ceb3338ba8ab9
Kim Stebel <kim.stebel@gmail.com>
2014-10-23 16:28:20 +0100

    default read access only for members of cloudant advocacy team

--- scripts/deploy-branch.sh
+++ scripts/deploy-branch.sh
@@ -10,7 +10,13 @@ then
     #deploy with db name == branch name
     couchapp push couchapp "https://$1:$2@docs-testb.cloudant.com/${TRAVIS_BRANCH}"
     #give everybody read access and docs-testb admin access
-    curl -X PUT "https://$1:$2@docs-testb.cloudant.com/${TRAVIS_BRANCH}/_security" -d '{"_id":"_security","cloudant":{"nobody":["_reader"],"docs-testb":["_reader","_writer","_admin","_replicator"]}}'
+    curl -X PUT "https://$1:$2@docs-testb.cloudant.com/${TRAVIS_BRANCH}/_security" -d '{"cloudant": {
+        "kimstebel": ["_reader"],
+        "warmana": ["_reader"],
+        "bradnoble": ["_reader"],
+        "reader": ["_reader"],
+        "bradley-holt": ["_reader"],
+        "docs-testb":["_reader","_writer","_admin","_replicator"]}}'
   fi
 else
   echo "this is a pull request; not deploying."
