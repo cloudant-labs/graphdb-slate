efd4c09dedfdfe947a6e276ba1c7ebce271f0075
Kim Stebel <kim.stebel@gmail.com>
2014-11-17 16:14:53 +0000

    give everybody access to the content-review branch

--- scripts/deploy-branch.sh
+++ scripts/deploy-branch.sh
@@ -9,17 +9,31 @@ then
     python scripts/upload_documentation_as_docs.py "$TRAVIS_BRANCH"
     #deploy with db name == branch name
     couchapp push couchapp "https://$1:$2@docs-testb.cloudant.com/${TRAVIS_BRANCH}"
-    #give everybody read access and docs-testb admin access
-    curl -X PUT "https://$1:$2@docs-testb.cloudant.com/${TRAVIS_BRANCH}/_security" -d '{"cloudant": {
-        "kimstebel": ["_reader"],
-        "warmanaibm": ["_reader"],
-        "bradnoble": ["_reader"],
-        "reader": ["_reader"],
-        "bradley-holt": ["_reader"],
-        "ten-eleven": ["_reader"],
-        "elsmore": ["_reader"],
-        "rajsingh": ["_reader"],
-        "docs-testb":["_reader","_writer","_admin","_replicator"]}}'
+    #give people read access and docs-testb admin access
+    security='
+      {
+        "cloudant": {
+		      "kimstebel": ["_reader"],
+		      "warmanaibm": ["_reader"],
+		      "bradnoble": ["_reader"],
+		      "reader": ["_reader"],
+		      "bradley-holt": ["_reader"],
+		      "ten-eleven": ["_reader"],
+		      "elsmore": ["_reader"],
+		      "rajsingh": ["_reader"],
+		      "docs-testb":["_reader","_writer","_admin","_replicator"]
+		    }
+		  }'
+    if [ "${TRAVIS_BRANCH}" = "content-review" ]; then
+      security='
+        {
+          "cloudant": {
+            "nobody":["_reader"],
+            "docs-testb":["_reader","_writer","_admin","_replicator"]
+          }
+        }'
+    fi
+    curl -X PUT "https://$1:$2@docs-testb.cloudant.com/${TRAVIS_BRANCH}/_security" -d "${security}"
   fi
 else
   echo "this is a pull request; not deploying."
