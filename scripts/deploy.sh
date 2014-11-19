8d36672894b8cb717505fcdcb89851e8238d3125
Kim Stebel <kim.stebel@gmail.com>
2014-11-19 16:48:01 +0000

    made deploy script more flexible

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
-if [ -z $1 ]
+if [ -z "$1" ]
 then
-  DEST='api-ref'
+  echo 'no db specified'
+  echo './deploy DB URL'
+  exit
 else
-  DEST=$1
+  DB="$1"
 fi
 
-couchapp push couchapp $DEST
+if [ -z "$2" ]
+then
+  echo 'no base url specified'
+  echo './deploy DB URL'
+else
+  URL="$2"
+fi
+
+python scripts/upload_documentation_as_docs.py "$DB"
+couchapp push couchapp "${URL}${DB}"
