a3464ad282758afa82eab4fa455402f89ec24c3f
Kim Stebel <kim.stebel@gmail.com>
2014-11-24 15:01:54 +0000

    search improvements: boost: 10 for titles and search with * at end of query

--- couchapp/indexes/docs/index.js
+++ couchapp/indexes/docs/index.js
@@ -1,5 +1,6 @@
 function (doc) {
   index('title', doc.title, { store: true });
   index('text', doc.text, { store: true });
-  index('default', doc.text);
+  index('default', doc.text, { boost: 1.0, store: true});
+  index('default', doc.title, { boost: 10.0, store: true});
 }
