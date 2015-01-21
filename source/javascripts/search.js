b5403ce339cc2b02b94d50c4bb979cb66cb40a02
Kim Stebel <kim.stebel@gmail.com>
2015-01-21 17:42:30 +0000

    search

--- source/javascripts/search.js
+++ source/javascripts/search.js
@@ -24,7 +24,7 @@ $(function () {
   }
 
   function on_submit(event) {
-    $.get(query_url, { q: query + '*' }).done(function (response) {
+    $.get(query_url, { q: $(query_input).val() + '*' }).done(function (response) {
       var res = JSON.parse(response);
       if (res.total_rows > 0) {
         on_select(null, res.rows[0], null);
