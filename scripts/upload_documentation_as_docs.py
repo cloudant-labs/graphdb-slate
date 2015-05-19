39a931e7b3f5408628d5dae792c0c75af1b8a384
Kim Stebel <kim.stebel@gmail.com>
2015-05-19 15:32:36 +0100

    fix bug in search deploy script

--- scripts/upload_documentation_as_docs.py
+++ scripts/upload_documentation_as_docs.py
@@ -21,58 +21,56 @@ print SECRET
 print "database" + DATABASE
 
 if KEY and SECRET:
-		print "account with auth"
-		account = cloudant.Account(USERNAME, auth=(KEY, SECRET))
+	print "account with auth"
+	account = cloudant.Account(USERNAME, auth=(KEY, SECRET))
 else:
-		print "account without auth"
-		account = cloudant.Account()
+  print "account without auth"
+  account = cloudant.Account()
 
 database = account.database(DATABASE)
 
-docs = dict()
-				
-
 def make_docs(headers, filename):
-		print "make_docs called with filename = " + filename
-		docs = dict()
-		for header in headers:
-				if not 'id' in header.attrs:
-						print 'header does not have an id attribute'
-						continue;
-				doc = dict(_id=filename + '-' + header['id'], title=header.get_text())
-				children = []
-				for sibling in header.next_siblings:
-						if sibling.name and sibling.name in HEADINGS:
-								break
-						elif sibling.string and not sibling.string == '\n':
-								children.append(sibling.string)
-						elif type(sibling) is NavigableString:
-								children.append(unicode(sibling))
-						else:
-								for s in sibling.strings:
-										children.append(s)
-				doc['text'] = ' '.join(children)
-				if doc['text']:
-						docs[doc['_id']] = doc
-		return docs
+	print "make_docs called with filename = " + filename
+	docs = dict()
+	for header in headers:
+		if not 'id' in header.attrs:
+			print 'header does not have an id attribute'
+			continue;
+		doc = dict(_id=filename + '-' + header['id'], title=header.get_text())
+		children = []
+		for sibling in header.next_siblings:
+			if sibling.name and sibling.name in HEADINGS:
+				break
+			elif sibling.string and not sibling.string == '\n':
+				children.append(sibling.string)
+			elif type(sibling) is NavigableString:
+				children.append(unicode(sibling))
+			else:
+				for s in sibling.strings:
+					children.append(s)
+		doc['text'] = ' '.join(children)
+		if doc['text']:
+			docs[doc['_id']] = doc
+	return docs
 
 def index_file(filename):
-		with open('build/' + filename + '.html', 'r') as f:
-				html = f.read()
-				soup = BeautifulSoup(html)
-				for heading in HEADINGS:
-						headers = soup.find_all(heading)
-						docs.update(make_docs(headers, filename))
-				#alldocs = database.all_docs().post(params=dict(keys=docs.keys()))
-				#alldocs.raise_for_status()
-				#for row in alldocs.json()['rows']:
-				#		if 'id' not in row:
-				#				continue
-				#		_id = row['id']
-				#		rev = row['value']['rev']
-				#		docs[_id]['_rev'] = rev
-				#response = database.bulk_docs(*docs.values())
-				#response.raise_for_status()
+	with open('build/' + filename + '.html', 'r') as f:
+		html = f.read()
+		soup = BeautifulSoup(html)
+		file_docs = dict()
+		for heading in HEADINGS:
+			headers = soup.find_all(heading)
+			file_docs.update(make_docs(headers, filename))
+		alldocs = database.all_docs().post(params=dict(keys=file_docs.keys()))
+		alldocs.raise_for_status()
+		for row in alldocs.json()['rows']:
+			if 'id' not in row:
+				continue
+			_id = row['id']
+			rev = row['value']['rev']
+			file_docs[_id]['_rev'] = rev
+		response = database.bulk_docs(*file_docs.values())
+		response.raise_for_status()
 				
 				
 				
@@ -114,9 +112,9 @@ for filename in [
 'try',
 'using_views'
 ]:
-		index_file(filename)
+	index_file(filename)
 		
 		
-with open('docs.json', 'wb') as jsonfile:
-	json.dump(docs, jsonfile)
+#with open('docs.json', 'wb') as jsonfile:
+	#json.dump(docs, jsonfile)
 				
